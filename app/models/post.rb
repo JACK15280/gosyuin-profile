edf1559edaec24537832d443d7656b95628e08f6
JACK15280 <junya1053@gmail.com>
2020-07-22 12:54:24 +0900

    create-search-area

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :group_posts, dependent: :destroy
   has_many :groups, through: :group_posts, dependent: :destroy
+
+  def self.search(search)
+    if search
+      Post.where(['title LIKE ? OR content LIKE ?', "%#{search}%", "%#{search}%"])
+    else
+      Post.all
+    end
+  end
 end
