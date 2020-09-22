079b0a3262a732904455c6060df970bb176e6c1d
JACK15280 <junya1053@gmail.com>
2020-09-22 18:58:55 +0900

    add-guest-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :groups
   has_many :comments
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
 end
