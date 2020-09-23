511de00e7ccc33c8e798e3e758dead0f6660e575
JACK15280 <junya1053@gmail.com>
2020-09-24 00:49:20 +0900

    add-status

--- db/migrate/20200717070039_create_posts.rb
+++ db/migrate/20200717070039_create_posts.rb
@@ -2,9 +2,10 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.integer :user_id
-      t.string :title
+      t.integer :status, null: false
+      t.string :title, null: false
       t.text :content
-      t.string :image
+      t.string :image, null: false
       t.timestamps
     end
   end
